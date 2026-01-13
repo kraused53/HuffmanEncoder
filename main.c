25c3cd6b1f4fe7be81e656085f05e5a84de69714
kraused53 <kraused53@gmail.com>
2026-01-13 06:34:41 -0500

    Moved all text related info into a struct
    This makes it easier to pass variables around

--- main.c
+++ main.c
@@ -2,10 +2,28 @@
 #include <stdlib.h>
 
 /* Text Info */
-const char* filepath = "./text_files/test1.txt";
-int character_freqs[ 256 ] = { 0 };
-unsigned char* text;
-int text_len;
+struct TextInfo {
+	int character_freqs[ 256 ];
+	unsigned char* text;
+	int text_len;
+};
+
+void init_TextInfo( struct TextInfo* ti ) {
+	for ( int i = 0; i < 256; i++ ) {
+		ti->character_freqs[ i ] = 0;
+	}
+	ti->text = NULL;
+	ti->text_len = 0;
+}
+
+void calc_char_freqs( struct TextInfo* ti ) {
+	// calculate character frequencies
+	for ( int i = 0; i < ti->text_len; i++ ) {
+		if ( ti->text[ i ] != '\0' ) {
+			ti->character_freqs[ ( unsigned char ) ti->text[ i ] ]++;
+		}
+	}
+}
 
 /* Binary Tree */
 
@@ -107,63 +125,79 @@ void delete_linked_list( struct LinkedListNode* head ) {
 	free( head );
 }
 
-int main( void ) {
-
+/* File Operations */
+int copy_file_to_text_info( const char* path, struct TextInfo* ti ) {
 	// Attempt to open the file "filepath"
 	FILE* fp = NULL;
-	fopen_s( &fp, filepath, "r" );
+	fopen_s( &fp, path, "r" );
 	if ( !fp ) {
-		printf( "Could not open file: %s\r\n", filepath );
+		printf( "Could not open file: %s\r\n", path );
 		return 1;
 	}
 
 	// Get length of file
 	fseek( fp, 0, SEEK_END );
-	text_len = ftell( fp );
+	ti->text_len = ftell( fp );
 	rewind( fp );
 
 	// Attempt to allocate buffer for file contents
-	text = calloc( text_len, sizeof( char ) );
-	if ( !text ) {
-		printf( "Could not allocate %d bytes of memory\r\n", text_len );
+	ti->text = calloc( ti->text_len, sizeof( char ) );
+	if ( !ti->text ) {
+		printf( "Could not allocate %d bytes of memory\r\n", ti->text_len );
 		fclose( fp );
 		return 1;
 	}
 
 	// Copy file to buffer
 	char c;
-	for ( int i = 0; i < text_len; i++ ) {
+	for ( int i = 0; i < ti->text_len; i++ ) {
 		c = fgetc( fp );
 		if ( c == EOF ) {
 			break;
 		}
-		text[ i ] = c;
+		ti->text[ i ] = c;
 	}
 
 	// Close file
 	fclose( fp );
 
-	// calculate character frequencies
-	for ( int i = 0; i < text_len; i++ ) {
-		if ( text[ i ] != '\0' ) {
-			character_freqs[ ( unsigned char ) text[ i ] ]++;
-		}
+	// No Errors
+	return 0;
+}
+
+int main( void ) {
+
+	// Set file to encode
+	const char* filepath = "./text_files/test1.txt";
+
+	// Set up text info
+	struct TextInfo to_encode;
+	init_TextInfo( &to_encode );
+
+	// Load text info
+	if ( copy_file_to_text_info( filepath, &to_encode ) != 0 ) {
+		return 1;
 	}
 	
+	// Calculate character frequencies
+	calc_char_freqs( &to_encode );
+
 	// Linked List
 	struct LinkedListNode* head = NULL;
 
 	for ( int i = 0; i < 256; i++ ) {
-		if ( character_freqs[ i ] != 0 ) {
-			head = append_node_sorted( head, character_freqs[ i ], i );
+		if ( to_encode.character_freqs[ i ] != 0 ) {
+			head = append_node_sorted( head, to_encode.character_freqs[ i ], i );
 		}
 	}
 
 	print_list( head );
 
+	
 	// Free memory
-	free( text );
+	free( to_encode.text );
 	delete_linked_list( head );
+	head = NULL;
 
 	return 0;
 }
